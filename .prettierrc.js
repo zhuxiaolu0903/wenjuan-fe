8ad031183d9231893f1aefcb5fd63512cc9f4915
“zhuxiaolu0903” <39010432+zhuxiaolu0903@users.noreply.github.com>
2023-06-28 05:49:44 +0000

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    tabWidth: 2,
+    useTabs: false,
+    singleQuote: true,
+    semi: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: true
+  }
