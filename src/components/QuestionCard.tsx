1f66574cc4d5fdd7f893b419a195f692896f66ec
zhuxiaolu0903 <1586752553@qq.com>
2023-08-20 22:11:59 +0800

    feat:完善我的问卷列表、星标问卷列表

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,16 +1,17 @@
 import React, { FC } from 'react'
 import styles from './QuestionCard.module.scss'
-import { Space, Badge } from 'antd'
+import { Space, Badge, Divider, Tag, Button, Tooltip, message } from 'antd'
 import {
   CopyOutlined,
   DeleteOutlined,
   EditOutlined,
   StarOutlined,
+  StarFilled,
   StockOutlined
 } from '@ant-design/icons'
 
 type PropsType = {
-  id: string
+  _id: string
   title: string
   isStar: boolean
   isPublished: boolean
@@ -19,55 +20,72 @@ type PropsType = {
 }
 
 const QuestionCard: FC<PropsType> = (props) => {
-  const { id, title, isPublished, answerCount, createdAt } = props
+  const { _id, title, isPublished, isStar, answerCount, createdAt } = props
+  const onCopy = () => {
+    message.success('复制成功!')
+  }
   return (
     <>
-      <Badge.Ribbon text={isPublished ? '已发布' : '未发布'}>
-        <div className={styles['container']}>
-          <div className={styles['title']}>
-            <div className={styles['left']}>
+      <div className={styles['container']}>
+        <div className={styles['title']}>
+          <div className={styles['left']}>
+            <Space>
               <a href="#">{title}</a>
-            </div>
-            <div className={styles['right']}>
-              <Space>
-                <span>答卷：{answerCount}</span>
-                <span>{createdAt}</span>
-              </Space>
-            </div>
+              <Tag color={isPublished ? 'green' : 'default'} bordered={false}>
+                {isPublished ? '已发布' : '未发布'}
+              </Tag>
+            </Space>
           </div>
-          <div className={styles['button-container']}>
-            <div className={styles['left']}>
-              {/*<Button icon={<EditOutlined />} type="text" size="small">编辑</Button>*/}
-              <Space>
-                <Space style={{ cursor: 'pointer' }}>
-                  <EditOutlined style={{ fontSize: '12px' }} />
-                  <span>编辑</span>
-                </Space>
-                <Space style={{ cursor: 'pointer' }}>
-                  <StockOutlined style={{ fontSize: '12px' }} />
-                  <span>数据统计</span>
-                </Space>
-              </Space>
-            </div>
-            <div className={styles['right']}>
-              <Space>
-                <Space style={{ cursor: 'pointer' }}>
-                  <StarOutlined style={{ fontSize: '12px' }} />
-                  <span>标星</span>
-                </Space>
-                <Space style={{ cursor: 'pointer' }}>
-                  <CopyOutlined style={{ fontSize: '12px' }} />
-                  <span>复制</span>
-                </Space>
-                <Space style={{ cursor: 'pointer' }}>
-                  <DeleteOutlined style={{ fontSize: '12px' }} />
-                  <span>删除</span>
-                </Space>
-              </Space>
-            </div>
+          <div className={styles['right']}>
+            {isStar ? (
+              <Tooltip title="取消标星">
+                <StarFilled style={{ color: '#fecd44', cursor: 'pointer', fontSize: '18px' }} />
+              </Tooltip>
+            ) : (
+              <Tooltip title="标星">
+                <StarOutlined style={{ color: '#fecd44', cursor: 'pointer', fontSize: '18px' }} />
+              </Tooltip>
+            )}
           </div>
         </div>
-      </Badge.Ribbon>
+        <div className={styles['question-info']}>
+          <span>创建时间：{createdAt}</span>
+          <span>
+            答卷：<span className={styles['number']}>{answerCount}</span>
+          </span>
+        </div>
+        <div className={styles['button-container']}>
+          <div className={styles['left']}>
+            <Space>
+              <Space style={{ cursor: 'pointer' }}>
+                <EditOutlined style={{ fontSize: '12px' }} />
+                <span>编辑</span>
+              </Space>
+              <Divider type="vertical" />
+              <Space
+                style={{ cursor: 'pointer' }}
+                className={isPublished ? '' : styles['disabled']}
+              >
+                <StockOutlined style={{ fontSize: '12px' }} />
+                <span>数据统计</span>
+              </Space>
+            </Space>
+          </div>
+          <div className={styles['right']}>
+            <Space>
+              <Space style={{ cursor: 'pointer' }} onClick={() => onCopy()}>
+                <CopyOutlined style={{ fontSize: '12px' }} />
+                <span>复制</span>
+              </Space>
+              <Divider type="vertical" />
+              <Space style={{ cursor: 'pointer' }}>
+                <DeleteOutlined style={{ fontSize: '12px' }} />
+                <span>删除</span>
+              </Space>
+            </Space>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/manage/Star.tsx
+++ src/pages/manage/Star.tsx
@@ -1,7 +1,73 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import QuestionCard from '../../components/QuestionCard'
+import styles from './common.module.scss'
+import { useTitle } from 'ahooks'
+import { Button, Divider, Empty, Space } from 'antd'
+import { SearchOutlined, SwapRightOutlined } from '@ant-design/icons'
+
+const rawQuestionList = [
+  {
+    _id: 'q1',
+    title: '问卷1',
+    isPublished: false,
+    isStar: true,
+    answerCount: 5,
+    createdAt: '2023/06/28'
+  },
+  {
+    _id: 'q2',
+    title: '问卷2',
+    isPublished: true,
+    isStar: true,
+    answerCount: 2,
+    createdAt: '2023/06/28'
+  }
+]
 
 const Star: FC = () => {
-  return <>Star</>
+  const [questionList, setQuestionList] = useState(rawQuestionList)
+  useTitle('问卷星球—星标问卷')
+
+  return (
+    <div className={styles['question-list-container']}>
+      <div className={styles['header']}>
+        <div className={styles['title']}>星标问卷</div>
+        <div className={styles['search']}>
+          <Space style={{ cursor: 'pointer' }}>
+            <SearchOutlined />
+            搜索
+          </Space>
+        </div>
+      </div>
+      <div className={styles['center']}>
+        {questionList.length === 0 ? (
+          <Empty
+            image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%'
+            }}
+            description={<span>您还没有问卷，请前往创建您的第一份问卷</span>}
+          >
+            <Button type="primary" icon={<SwapRightOutlined />}>
+              立即前往
+            </Button>
+          </Empty>
+        ) : (
+          questionList.map((item, index) => (
+            <div key={item._id}>
+              <QuestionCard {...item} />
+              {index !== questionList.length - 1 && <Divider style={{ margin: '16px 0 8px 0' }} />}
+            </div>
+          ))
+        )}
+      </div>
+      <div className={styles['footer']}>分页</div>
+    </div>
+  )
 }
 
 export default Star
